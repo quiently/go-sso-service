2017fef219de947c2d2f9c8fb128aa336cdb88a9
Uladzislau Shyian <UladzislauShyian@gmail.com>
2025-02-08 21:27:19 +0300

    [ref]: +

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/quiently/go-sso-service/internal/config"
+	"sync"
 )
 
+// import (
+// 	"fmt"
+
+// 	"github.com/quiently/go-sso-service/internal/config"
+// )
+
+// func main() {
+// 	config := config.MustLoad()
+// 	fmt.Println(config)
+
+// }
+var msg string
+
+func updateMessage(s string) {
+	msg = s
+}
+
+func printMessage() {
+	fmt.Println(msg)
+}
+
 func main() {
-	config := config.MustLoad()
-	fmt.Println(config)
+	var wg sync.WaitGroup
+	msg = "Hello, world!"
+
+
+	message("Hello, universe!", &wg)
+	message("Hello, cosmos!", &wg)
+	message("Hello, world!", &wg)
+}
+
+func message(message string, wg *sync.WaitGroup) {
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		updateMessage(message)
+	}()
 }
